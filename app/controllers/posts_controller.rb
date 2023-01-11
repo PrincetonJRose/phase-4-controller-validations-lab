b17e99c75293034f95f430e4b3d6dd47fe4b8075
Princeton J. Rose <princetonjrose@gmail.com>
2023-01-11 13:13:36 -0500

    finished review

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     post = Post.find(params[:id])
     
@@ -9,7 +12,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +21,15 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def render_not_found_response
+    render json: { errors: ['Post was not found.'] }, status: 404
+  end
+  
+  def render_unprocessable_entity_response post
+    # render json: { errors: author.record.errors.full_messages }, status: 422
+    render json: { errors: post.record.errors.messages }, status: 422
   end
 
 end
