8de1d378ffac857218ba99ab6c7f166d9b99863b
Princeton J. Rose <princetonjrose@gmail.com>
2023-01-11 13:19:04 -0500

    added in to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+
+    # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+
+    # def render_not_found_response
+    #     render json: { errors: ['Record was not found.'] }, status: 404
+    # end
 end
